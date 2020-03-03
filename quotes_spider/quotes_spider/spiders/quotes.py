de9a70fa6bebce3fa6ce41a836b0140f476a7ae3
Danish Riaz <danishriaz0609@outlook.com>
2020-03-03 11:50:12 +0500

    Scarpy, quotes scraped from all pages and edites to csv-json-xml

--- quotes_spider/quotes_spider/spiders/quotes.py
+++ quotes_spider/quotes_spider/spiders/quotes.py
@@ -3,15 +3,28 @@ import scrapy
 
 
 class QuotesSpider(scrapy.Spider):
+
     name = 'quotes'
     allowed_domains = ['quotes.toscrape.com']
     start_urls = ['http://quotes.toscrape.com/']
 
     def parse(self, response):
-        h1_tag = response.xpath('//h1/a/text()').extract_first()
-        right_side_tags = response.xpath('//*[@class="tag-item"]/a/text()').extract()
+        quotes = response.xpath('//*[@class="quote"]')
+        for quote in quotes:
+            text = quote.xpath(".//*[@class='text']/text()").extract_first()
+            author = quote.xpath(
+                ".//*[@class='author']/text()").extract_first()
+            tags = quote.xpath(
+                ".//*[@itemprop='keywords']/@content").extract_first()
 
-        yield{
-        'H1_Tags' : h1_tag,
-        'Tags' : right_side_tags
-        }
+            yield{
+                'Text': text,
+                'Author': author,
+                'Tags': tags
+            }
+
+        next_page = response.xpath(
+            ".//*[@class='next']/a/@href").extract_first()
+        abs_next_page = response.urljoin(next_page)
+
+        yield scrapy.Request(abs_next_page)
