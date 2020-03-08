c2a46dc8b71a84c4f58433d98a602ac179aecb5a
Danish Riaz <danishriaz0609@outlook.com>
2020-03-08 11:01:19 +0500

    Scrapy, what_mobile spider,items.py done adding pipeline

--- what_mobile/what_mobile/items.py
+++ what_mobile/what_mobile/items.py
@@ -5,10 +5,49 @@
 # See documentation in:
 # https://doc.scrapy.org/en/latest/topics/items.html
 
-import scrapy
+from scrapy import Item
 
 
-class WhatMobileItem(scrapy.Item):
+class WhatMobileItem(Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    pass
+    mobile_name = scrapy.Field()
+    price_pkr = scrapy.Field()
+    price_usd = scrapy.Field()
+    rating = scrapy.Field()
+    os = scrapy.Field()
+    ui = scrapy.Field()
+    dimensions = scrapy.Field()
+    weight = scrapy.Field()
+    sim = scrapy.Field()
+    colors = scrapy.Field()
+    band_2G = scrapy.Field()
+    band_3G = scrapy.Field()
+    band_4G = scrapy.Field()
+    cpu = scrapy.Field()
+    chipset = scrapy.Field()
+    gpu = scrapy.Field()
+    technology = scrapy.Field()
+    size = scrapy.Field()
+    resolution = scrapy.Field()
+    protection = scrapy.Field()
+    extra_features = scrapy.Field()
+    built_in = scrapy.Field()
+    card = scrapy.Field()
+    main = scrapy.Field()
+    features = scrapy.Field()
+    front = scrapy.Field()
+    wlan = scrapy.Field()
+    bluetooth = scrapy.Field()
+    gps = scrapy.Field()
+    usb = scrapy.Field()
+    nfc = scrapy.Field()
+    data = scrapy.Field()
+    sensors = scrapy.Field()
+    audio = scrapy.Field()
+    browser = scrapy.Field()
+    messaging = scrapy.Field()
+    games = scrapy.Field()
+    torch = scrapy.Field()
+    extra = scrapy.Field()
+    capacity = scrapy.Field()
--- what_mobile/what_mobile/spiders/mobile.py
+++ what_mobile/what_mobile/spiders/mobile.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 from scrapy import Spider
 from scrapy.http import Request
+from scrapy.loader import ItemLoader
+from what_mobile.item import WhatMobileItem
 
 
 def first_element(response, name):
@@ -51,16 +53,100 @@ class MobileSpider(Spider):
         #     yield Request(mobile_url, callback=self.mobile_data)
 
     def mobile_data(self, response):
+
+        l = ItemLoader(text=WhatMobileItem(), response=response)
+
         mobile_name = response.xpath(
             '//h1[@class="hdng3"]/text()').extract_first()
         price_pkr = response.xpath('//*[@class="hdng"]/text()')[0].extract()
         price_usd = response.xpath('//*[@class="hdng"]/text()')[1].extract()
         rating = response.xpath(
             '//*[contains(@class, "rateit")]/@data-rateit-value')[0].extract()
+        # Build
         os = first_element(response, "OS")
         ui = after_first_element(response, "UI")
         dimensions = after_first_element(response, "Dimensions")
+        weight = after_first_element(response, "Weight")
+        sim = after_first_element(response, "SIM")
+        colors = after_first_element(response, "Colors")
+        # Frequency
+        band_2G = first_element(response, "2G Band")
+        band_3G = after_first_element(response, "3G Band")
+        band_4G = after_first_element(response, "4G Band")
+        # Processor
+        cpu = first_element(response, "CPU")
+        chipset = after_first_element(response, "Chipset")
+        gpu = after_first_element(response, "GPU")
+        # Display
+        technology = first_element(response, "Technology")
+        size = after_first_element(response, "Size")
+        resolution = after_first_element(response, "Resolution")
+        protection = after_first_element(response, "Protection")
+        extra_features = after_first_element(response, "Extra Features")
+        # Memory
+        built_in = first_element(response, "Built-in")
+        card = after_first_element(response, "Card")
+        # Camera
+        main = first_element(response, "Main")
+        features = after_first_element(response, "Features")
+        front = after_first_element(response, "Front")
+        # Connectivity
+        wlan = first_element(response, "WLAN")
+        bluetooth = after_first_element(response, "Bluetooth")
+        gps = after_first_element(response, "GPS")
+        usb = after_first_element(response, "USB")
+        nfc = after_first_element(response, "NFC")
+        data = after_first_element(response, "Data")
+        # Features
+        sensors = first_element(response, "Sensors")
+        audio = after_first_element(response, "Audio")
+        browser = after_first_element(response, "Browser")
+        messaging = after_first_element(response, "Messaging")
+        games = after_first_element(response, "Games")
+        torch = after_first_element(response, "Torch")
+        extra = after_first_element(response, "Extra")
+        # Battery
+        capacity = first_element(response, "Capacity")
 
-        yield{'Mobile_Name': mobile_name,
-              'PRICE PKR': price_pkr,
-              'PRICE USD': price_usd}
+        l.add_value('mobile_name', mobile_name)
+        l.add_value('price_pkr', price_pkr)
+        l.add_value('price_usd', price_usd)
+        l.add_value('rating', rating)
+        l.add_value('os', os)
+        l.add_value('ui', ui)
+        l.add_value('dimensions', dimensions)
+        l.add_value('weight', weight)
+        l.add_value('sim', sim)
+        l.add_value('colors', colors)
+        l.add_value('band_2G', band_2G)
+        l.add_value('band_3G', band_3G)
+        l.add_value('band_4G', band_4G)
+        l.add_value('cpu', cpu)
+        l.add_value('chipset', chipset)
+        l.add_value('gpu', gpu)
+        l.add_value('technology', technology)
+        l.add_value('size', size)
+        l.add_value('resolution', resolution)
+        l.add_value('protection', protection)
+        l.add_value('extra_features', extra_features)
+        l.add_value('built_in', built_in)
+        l.add_value('card', card)
+        l.add_value('main', main)
+        l.add_value('features', features)
+        l.add_value('front', front)
+        l.add_value('wlan', wlan)
+        l.add_value('bluetooth', bluetooth)
+        l.add_value('gps', gps)
+        l.add_value('usb', usb)
+        l.add_value('nfc', nfc)
+        l.add_value('data', data)
+        l.add_value('sensors', sensors)
+        l.add_value('audio', audio)
+        l.add_value('browser', browser)
+        l.add_value('messaging', messaging)
+        l.add_value('games', games)
+        l.add_value('torch', torch)
+        l.add_value('extra', extra)
+        l.add_value('capacity', capacity)
+
+        return l.load_item()
