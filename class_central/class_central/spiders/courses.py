f8c9c14cb3235a880f81f28379bc1d2483993574
Danish Riaz <danishriaz0609@outlook.com>
2020-03-11 14:05:43 +0500

    Scrapy, class_central, changing logic do to loop issue or itration indepent

--- class_central/class_central/spiders/courses.py
+++ class_central/class_central/spiders/courses.py
@@ -5,7 +5,7 @@ from scrapy.http import Request
 
 class CoursesSpider(Spider):
     name = 'courses'
-    allowed_domains = ['classcentral.com/subjects']
+    allowed_domains = ['classcentral.com']
     start_urls = ['https://www.classcentral.com/subjects']
 
     def __init__(self, subject=None):
@@ -14,11 +14,22 @@ class CoursesSpider(Spider):
     def parse(self, response):
         if self.subject:
             url = response.xpath(
-                '//a[contains(@title, "+subject+")]/@href').get()
+                '//a[contains(@title, "' + self.subject + '")]/@href').extract_first()
             url = 'https://www.classcentral.com' + url
             yield Request(url, callback=self.get_data)
         else:
             print("False")
 
-    def get_data():
-        pass
+    def get_data(self, response):
+        div = response.xpath(
+            '//tr[@class="row nowrap vert-align-middle padding-vert-small border-bottom border-gray-light"]')
+
+        title = div.xpath('.//td[2]/a/@title').extract()
+        url = div.xpath('.//td[2]/a/@href').extract()
+        rating = response.xpath(
+            '//td[@class="hide-on-hover fill-space relative"]/@data-timestamp').extract()
+
+        yield{'title': title,
+              'url': url,
+              'rating': rating
+              }
