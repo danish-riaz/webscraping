ccbead8eaee988e490c6b9febb0271ad526e40c8
Danish Riaz <danishriaz0609@outlook.com>
2020-03-14 08:20:30 +0500

    Scrapy, class-central, Scraped indiviual subjects successfully using efficient technique

--- class_central/class_central/spiders/courses.py
+++ class_central/class_central/spiders/courses.py
@@ -16,44 +16,58 @@ class CoursesSpider(Spider):
         if self.subject:
             url = response.xpath(
                 '//a[contains(@title, "' + self.subject + '")]/@href').extract_first()
-            url = 'https://www.classcentral.com' + url
-            yield Request(url, callback=self.get_data, dont_filter=True)
+            abs_url = 'https://www.classcentral.com' + url
+            yield Request(abs_url, callback=self.get_data, meta={'subject': url})
         else:
             subjects = response.xpath(
-                '//a[@class="border-box align-middle color-charcoal hover-no-underline"]/span[1]/text()').extract()
+                '//a[@class="border-box align-middle color-charcoal hover-no-underline"]/@href').extract()
             subjects = set(subjects)
             for subject in subjects:
-                url = response.xpath(
-                    '//a[contains(@title, "' + subject + '")]/@href').extract_first()
-                url = 'https://www.classcentral.com' + url
-                self.logger.info(
-                    "############################# INSIDE SUBJECT ############################")
-                self.subject = subject
-                yield Request(url, callback=self.get_data, dont_filter=True)
-                self.subject = subject
+                url = 'https://www.classcentral.com' + subject
+                yield Request(url, callback=self.get_data, meta={'subject': subject})
 
     def get_data(self, response):
-        self.logger.info(
-            "############################# GETTING DATA ############################")
         div = response.xpath(
             '//tr[@class="row nowrap vert-align-middle padding-vert-small border-bottom border-gray-light"]')
 
         title = div.xpath('.//td[2]/a/@title').extract()
         url = div.xpath('.//td[2]/a/@href').extract()
+        category = response.xpath(
+            '//h1[@class="head-1"]/text()').extract_first()
         rating = response.xpath(
             '//td[@class="hide-on-hover fill-space relative"]/@data-timestamp').extract()
+
         for a, b, c in zip(title, url, rating):
             yield{'Title': a,
                   'Url': ('https://www.classcentral.com' + b),
                   'Rating': c,
-                  'Subject': self.subject
+                  'Subject': category
                   }
-        # load_more = response.xpath(
-        #     '//button[@class="btn-blue-outline btn-large margin-top-medium text-center"]/span[1]/text()').extract_first()
-        # if load_more:
-        #     self.counter += 1
-        #     url = 'https://www.classcentral.com/subject/' + self.subject + '?page=' + \
-        #         str(self.counter)
-        #     self.logger.info(
-        #         "############################# LOAD MORE ############################")
-        #     yield Request(url, callback=self.get_data)
+
+        load_more = response.xpath(
+            '//button[@class="btn-blue-outline btn-large margin-top-medium text-center"]/span[1]/text()').extract_first()
+
+        if load_more:
+            self.counter += 1
+            joiner = '?page=' + str(self.counter)
+            url_subject = response.meta.get('subject')
+            url = 'https://www.classcentral.com' + str(url_subject) + joiner
+            print("##############################################################")
+            print(url)
+            print("##############################################################")
+            yield Request(url, callback=self.get_data, meta={'subject': url_subject})
+        else:
+            print(
+                "##########################################################################")
+            print(
+                "##########################################################################")
+            print(
+                "##########################################################################")
+            print("END")
+            print(
+                "##########################################################################")
+            print(
+                "##########################################################################")
+            print(
+                "##########################################################################")
+            self.counter = 0
