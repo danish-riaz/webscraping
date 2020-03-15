01fb2f2e008899d3086649ce49452145ea7b7af6
Danish Riaz <danishriaz0609@outlook.com>
2020-03-15 10:47:07 +0500

    Scrapy, class-central, still facing load_more button issue also found start_url listing technique

--- class_central/class_central/spiders/courses.py
+++ class_central/class_central/spiders/courses.py
@@ -6,27 +6,28 @@ from scrapy.http import Request
 class CoursesSpider(Spider):
     name = 'courses'
     allowed_domains = ['classcentral.com']
-    start_urls = ['https://www.classcentral.com/subjects']
+    start_urls = ['https://www.classcentral.com/subject/cs',
+                  'https://www.classcentral.com/subject/engineering']
 
     def __init__(self, subject=None):
         self.subject = subject
-        self.counter = 1
+        self.counter = 0
+    # def parse(self, response):
+    #     if self.subject:
+    #         url = response.xpath(
+    #             '//a[contains(@title, "' + self.subject + '")]/@href').extract_first()
+    #         abs_url = 'https://www.classcentral.com' + url
+    #         yield Request(abs_url, callback=self.get_data, meta = {'subject' : url})
+    #     else:
+    #         subjects = response.xpath(
+    #             '//a[@class="border-box align-middle color-charcoal hover-no-underline"]/@href').extract()
+    #         subjects = set(subjects)
+    #         for subject in subjects:
+    #             url = 'https://www.classcentral.com' + subject
+    #             yield Request(url, callback=self.get_data)
 
     def parse(self, response):
-        if self.subject:
-            url = response.xpath(
-                '//a[contains(@title, "' + self.subject + '")]/@href').extract_first()
-            abs_url = 'https://www.classcentral.com' + url
-            yield Request(abs_url, callback=self.get_data, meta={'subject': url})
-        else:
-            subjects = response.xpath(
-                '//a[@class="border-box align-middle color-charcoal hover-no-underline"]/@href').extract()
-            subjects = set(subjects)
-            for subject in subjects:
-                url = 'https://www.classcentral.com' + subject
-                yield Request(url, callback=self.get_data, meta={'subject': subject})
 
-    def get_data(self, response):
         div = response.xpath(
             '//tr[@class="row nowrap vert-align-middle padding-vert-small border-bottom border-gray-light"]')
 
@@ -43,7 +44,6 @@ class CoursesSpider(Spider):
                   'Rating': c,
                   'Subject': category
                   }
-
         load_more = response.xpath(
             '//button[@class="btn-blue-outline btn-large margin-top-medium text-center"]/span[1]/text()').extract_first()
 
@@ -52,22 +52,7 @@ class CoursesSpider(Spider):
             joiner = '?page=' + str(self.counter)
             url_subject = response.meta.get('subject')
             url = 'https://www.classcentral.com' + str(url_subject) + joiner
-            print("##############################################################")
-            print(url)
-            print("##############################################################")
             yield Request(url, callback=self.get_data, meta={'subject': url_subject})
         else:
-            print(
-                "##########################################################################")
-            print(
-                "##########################################################################")
-            print(
-                "##########################################################################")
-            print("END")
-            print(
-                "##########################################################################")
-            print(
-                "##########################################################################")
-            print(
-                "##########################################################################")
+
             self.counter = 0
