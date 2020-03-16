422b3f98c3a85c3d9ccb4485d4c914788b99cf5a
Danish Riaz <danishriaz0609@outlook.com>
2020-03-16 09:48:28 +0500

    Scrapy, added proxies

--- class_central/class_central/spiders/courses.py
+++ class_central/class_central/spiders/courses.py
@@ -1,33 +1,42 @@
 # -*- coding: utf-8 -*-
 from scrapy import Spider
 from scrapy.http import Request
-from scrapy.http import FormRequest
 import math
 
 
 class CoursesSpider(Spider):
     name = 'courses'
     allowed_domains = ['classcentral.com']
-    start_urls = ['https://www.classcentral.com/subject/cs']
-
-    def __init__(self, subject=None):
-        self.subject = subject
-        self.page = 0
-    # def parse(self, response):
-    #     if self.subject:
-    #         url = response.xpath(
-    #             '//a[contains(@title, "' + self.subject + '")]/@href').extract_first()
-    #         abs_url = 'https://www.classcentral.com' + url
-    #         yield Request(abs_url, callback=self.get_data, meta = {'subject' : url})
-    #     else:
-    #         subjects = response.xpath(
-    #             '//a[@class="border-box align-middle color-charcoal hover-no-underline"]/@href').extract()
-    #         subjects = set(subjects)
-    #         for subject in subjects:
-    #             url = 'https://www.classcentral.com' + subject
-    #             yield Request(url, callback=self.get_data)
+    start_urls = ['https://www.classcentral.com/subject/cs',
+                  'https://www.classcentral.com/subject/business',
+                  'https://www.classcentral.com/subject/humanities',
+                  'https://www.classcentral.com/subject/data-science',
+                  'https://www.classcentral.com/subject/personal-development',
+                  'https://www.classcentral.com/subject/art-and-design',
+                  'https://www.classcentral.com/subject/programming-and-software-development',
+                  'https://www.classcentral.com/subject/engineering',
+                  'https://www.classcentral.com/subject/health',
+                  'https://www.classcentral.com/subject/maths',
+                  'https://www.classcentral.com/subject/science',
+                  'https://www.classcentral.com/subject/social-sciences',
+                  'https://www.classcentral.com/subject/education',
+                  ]
 
     def parse(self, response):
+        self.current_sub = response.url
+        self.current_sub = self.current_sub.split('/')[4]
+        yield Request(response.url, callback=self.get_data)
+
+    def get_data(self, response):
+        course_count = response.xpath(
+            '//h2[@class="text-1"]/text()').re('[0-9]')
+        course_count = int(''.join(course_count))
+        for page in range(0, (math.ceil(course_count / 50))):
+            next_url = 'https://www.classcentral.com/subject/' + self.current_sub + '?page=%s' % str(
+                page + 1)
+            yield Request(next_url, callback=self.loop_thorugh)
+
+    def loop_thorugh(self, response):
 
         div = response.xpath(
             '//tr[@class="row nowrap vert-align-middle padding-vert-small border-bottom border-gray-light"]')
@@ -39,35 +48,10 @@ class CoursesSpider(Spider):
         rating = response.xpath(
             '//td[@class="hide-on-hover fill-space relative"]/@data-timestamp').extract()
 
-        course_count = response.xpath(
-            '//h2[@class="text-1"]/text()').re('[0-9]')
-        course_count = int(''.join(course_count))
-
         for a, b, c in zip(title, url, rating):
             yield{'Title': a,
                   'Url': ('https://www.classcentral.com' + b),
                   'Rating': c,
-                  'Subject': category
+                  'Subject': category,
+                  'Global_URL': response.url
                   }
-
-        # load_more = response.xpath(
-        #     '//button[@class="btn-blue-outline btn-large margin-top-medium text-center"]/span[1]/text()').extract_first()
-        # while True:
-        #     if load_more:
-        for page in range(2, (math.ceil(course_count / 50))):
-            try:
-                next_url = 'https://www.classcentral.com/subject/cs?page=%s' % str(
-                    page + 1)
-                yield Request(next_url, callback=self.parse)
-            except:
-                pass
-            # else:
-            #     break
-        #     self.counter += 1
-        #     joiner = '?page=' + str(self.counter)
-        #     url_subject = response.meta.get('subject')
-        #     url = 'https://www.classcentral.com' + str(url_subject) + joiner
-        #     yield Request(url, callback=self.get_data, meta={'subject': url_subject})
-        # else:
-
-        #     self.counter = 0
