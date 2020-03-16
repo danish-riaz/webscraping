1a4ccd30253b7124b11c29bc7b2f06a298971b11
Danish Riaz <danishriaz0609@outlook.com>
2020-03-16 05:38:10 +0500

    Scrapy, class-central, Done with one subject just have to loop over all subjects

--- class_central/class_central/spiders/courses.py
+++ class_central/class_central/spiders/courses.py
@@ -1,17 +1,18 @@
 # -*- coding: utf-8 -*-
 from scrapy import Spider
 from scrapy.http import Request
+from scrapy.http import FormRequest
+import math
 
 
 class CoursesSpider(Spider):
     name = 'courses'
     allowed_domains = ['classcentral.com']
-    start_urls = ['https://www.classcentral.com/subject/cs',
-                  'https://www.classcentral.com/subject/engineering']
+    start_urls = ['https://www.classcentral.com/subject/cs']
 
     def __init__(self, subject=None):
         self.subject = subject
-        self.counter = 0
+        self.page = 0
     # def parse(self, response):
     #     if self.subject:
     #         url = response.xpath(
@@ -38,21 +39,35 @@ class CoursesSpider(Spider):
         rating = response.xpath(
             '//td[@class="hide-on-hover fill-space relative"]/@data-timestamp').extract()
 
+        course_count = response.xpath(
+            '//h2[@class="text-1"]/text()').re('[0-9]')
+        course_count = int(''.join(course_count))
+
         for a, b, c in zip(title, url, rating):
             yield{'Title': a,
                   'Url': ('https://www.classcentral.com' + b),
                   'Rating': c,
                   'Subject': category
                   }
-        load_more = response.xpath(
-            '//button[@class="btn-blue-outline btn-large margin-top-medium text-center"]/span[1]/text()').extract_first()
 
-        if load_more:
-            self.counter += 1
-            joiner = '?page=' + str(self.counter)
-            url_subject = response.meta.get('subject')
-            url = 'https://www.classcentral.com' + str(url_subject) + joiner
-            yield Request(url, callback=self.get_data, meta={'subject': url_subject})
-        else:
+        # load_more = response.xpath(
+        #     '//button[@class="btn-blue-outline btn-large margin-top-medium text-center"]/span[1]/text()').extract_first()
+        # while True:
+        #     if load_more:
+        for page in range(2, (math.ceil(course_count / 50))):
+            try:
+                next_url = 'https://www.classcentral.com/subject/cs?page=%s' % str(
+                    page + 1)
+                yield Request(next_url, callback=self.parse)
+            except:
+                pass
+            # else:
+            #     break
+        #     self.counter += 1
+        #     joiner = '?page=' + str(self.counter)
+        #     url_subject = response.meta.get('subject')
+        #     url = 'https://www.classcentral.com' + str(url_subject) + joiner
+        #     yield Request(url, callback=self.get_data, meta={'subject': url_subject})
+        # else:
 
-            self.counter = 0
+        #     self.counter = 0
