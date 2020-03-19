8cebf2582089b8b1818f9a98171a3fe7401e922e
Danish Riaz <danishriaz0609@outlook.com>
2020-03-19 07:45:56 +0500

    Scray, amazon_books, writing logic for bookname, authro and img only

--- amazon_books/amazon_books/spiders/books.py
+++ amazon_books/amazon_books/spiders/books.py
@@ -3,6 +3,7 @@ import scrapy
 from amazon_books.items import AmazonBooksItem
 from scrapy.loader import ItemLoader
 from scrapy.loader.processors import MapCompose
+from scrapy.http import Request
 
 
 class BooksSpider(scrapy.Spider):
@@ -12,19 +13,23 @@ class BooksSpider(scrapy.Spider):
         'https://www.amazon.com/s?rh=n%3A283155%2Cn%3A%211000%2Cn%3A3&page=2&qid=1584500043&ref=lp_3_pg_2']
 
     def parse(self, response):
-        l = ItemLoader(item=AmazonBooksItem(), response=response)
         rows = response.xpath(
             '//div[@class="s-include-content-margin s-border-bottom s-latency-cf-section"]')
         for row in rows:
-            book_name = row.xpath(
-                './/span[@class="a-size-medium a-color-base a-text-normal"]/text()').extract_first()
-            img = row.xpath(
-                './/div[@class="a-section aok-relative s-image-fixed-height"]/img/@src').extract_first()
-            author = row.xpath(
-                './/div[@class="a-row a-size-base a-color-secondary"]/a[@class="a-size-base a-link-normal"]/text()').extract()
+            yield Request(response.url, callback=self.get_data, meta={'row': row}, dont_filter=True)
 
-            l.add_value('book_name', book_name, MapCompose(str.strip))
-            l.add_value('img', img, MapCompose(str.strip))
-            l.add_value('author', author, MapCompose(str.strip))
+    def get_data(self, response):
+        l = ItemLoader(item=AmazonBooksItem(), response=response)
+        row = response.meta.get('row')
+        book_name = row.xpath(
+            './/span[@class="a-size-medium a-color-base a-text-normal"]/text()').extract_first()
+        img = row.xpath(
+            './/div[@class="a-section aok-relative s-image-fixed-height"]/img/@src').extract_first()
+        author = row.xpath(
+            './/div[@class="a-row a-size-base a-color-secondary"]/span[@dir="auto" and text()="by "][1]/following-sibling::*[1]/text()').extract()
 
-            yield l.load_item()
+        l.add_value('book_name', book_name, MapCompose(str.strip))
+        l.add_value('img', img, MapCompose(str.strip))
+        l.add_value('author', author, MapCompose(str.strip))
+
+        return l.load_item()
